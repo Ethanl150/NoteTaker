6550577de7cd06dd770838e5d1e74506f0a7b60f
Ethan Lacy <ethanr0m818@gmail.com>
2020-03-05 22:25:24 -0500

    added slash route

--- Develop/server.js
+++ Develop/server.js
@@ -20,6 +20,10 @@ app.use(express.json());
 
 app.use(express.static(__dirname));
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
